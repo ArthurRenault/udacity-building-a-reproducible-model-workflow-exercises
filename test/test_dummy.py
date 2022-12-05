249cf3f063e2c0c300996379b54a6d52f868ecc7
Arthur Renault <arthur.renault@telecom-paristech.fr>
2022-12-05 13:28:38 +0100

    ENH: Update CI pipeline

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,6 +1,24 @@
-def test_dummy_fails():
-    assert False
+import numpy as np
+import pandas as pd
 
 
 def test_dummy():
+    assert True
+
+
+def test_import_success():
+    import pandas
+    import numpy
+
+    pandas.DataFrame()
+    numpy.array([])
+
+    assert True
+
+
+def test_import_fail():
+    import scipy
+
+    print(scipy.stats)
+
     assert True
